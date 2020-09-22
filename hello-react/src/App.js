a15fa28924b57ab78129a52fae175ca8204a7ab0
SOSEULBI <43867643+padakpadak@users.noreply.github.com>
2020-09-22 14:17:08 +0900

    add hello-react subfolder

---
+++ hello-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import MyName from './Myname';
+//import logo from './logo.svg';
+//import './App.css';
+import Counter from './Counter';
+
+class App extends Component {
+  render(){
+    return(
+      <Counter/>
+    )
+    
+  }
+}
+
+export default App;
